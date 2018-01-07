b0fcadd5aebcd14124c048717a90835063ff74bf
Jesse Zelaya <jessezelaya1@gmail.com>
2018-01-06 16:52:13 -0800

    App skeleton set up
    Base structure and stylings for app

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Firebase from "./firebase";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+            <div className='wrapper'>
+              <h1>Potluck App</h1>
+            </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='container'>
+          <section className='add-item'>
+              <form>
+                <input type="text" name="username" placeholder="What's your name?" />
+                <input type="text" name="currentItem" placeholder="What are you bringing?" />
+                <button>Add Item</button>
+              </form>
+          </section>
+          <section className='display-item'>
+            <div className='wrapper'>
+              <ul>
+              </ul>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
